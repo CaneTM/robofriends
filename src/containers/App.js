ff4186d48a394dfb7ca9e3d8b506af178d9f52c9
CaneTM <canaantm@gmail.com>
2022-07-08 14:02:32 -0400

    Implemented React hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,78 +1,88 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import CardsDisplay from "../components/CardsDisplay";
 import ProfileDisplay from "../components/ProfileDisplay";
 import ErrorBoundary from "../components/ErrorBoundary";
 
 
-// a "smart" component, since it has state
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            robots: [],
-            searchfield: "",
-            isRobotClicked: false,
-            clickedRobotId: 0
-        };
-    }
+function App() {
+    // constructor() {
+    //     super();
+    //     this.state = {
+    //         robots: [],
+    //         searchfield: "",
+    //         isRobotClicked: false,
+    //         clickedRobotId: 0
+    //     };
+    // }
+
+    const [robots, setRobots] = useState([]);
+    const [searchfield, setSearchfield] = useState("");
+    const [isRobotClicked, setIsRobotClicked] = useState(false);
+    const [clickedRobotId, setClickedRobotId] = useState(0);
 
     // use arrow functions so "this" refers to App class, not input tag
-    onSearchChange = event => {
-        this.setState({ searchfield: event.target.value });
+    // disregard above comment
+    const onSearchChange = event => {
+        setSearchfield(event.target.value);
     }
 
-    onCardClick = event => {
-        this.setState({ clickedRobotId: event.currentTarget.id });
-        this.setState({ isRobotClicked: true });
+    const onCardClick = event => {
+        setClickedRobotId(event.currentTarget.id);
+        setIsRobotClicked(true);
     }
 
-    onGoBack = event => {
-        this.setState({ searchfield: "" });
-        this.setState({ clickedRobotId: 0 })
-        this.setState({ isRobotClicked: false });
+    const onGoBack = event => {
+        setSearchfield("");
+        setClickedRobotId(0);
+        setIsRobotClicked(false);
     }
 
-    componentDidMount() {
+    // componentDidMount() {
+    //     fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({ robots: users }));
+    // }
+
+    // gets called after every render,
+    // but when an empty array is provided, it is treated
+    // like componentDidMount (only runs once after intial render)
+    useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
-        .then(users => this.setState({ robots: users }));
-    }
+        .then(users => setRobots(users));
+    }, []);
 
-    render() {
-        const { robots, searchfield, clickedRobotId, isRobotClicked } = this.state;
+    // all strings include an empty string 
+    const filteredRobots = !clickedRobotId ? 
+        robots.filter(robot => robot.name.toLowerCase().includes(searchfield.toLowerCase())) :
+        robots.filter(robot => robot.id === Number(clickedRobotId));
 
-        // all strings include an empty string 
-        const filteredRobots = !clickedRobotId ? 
-            robots.filter(robot => robot.name.toLowerCase().includes(searchfield.toLowerCase())) :
-            robots.filter(robot => robot.id === Number(clickedRobotId));
-
-        if (!robots.length) {
-            return <h1 className="tc">Loading . . .</h1>
-        } 
-        else if (filteredRobots.length === 1) {
-            const robotUser = filteredRobots[0];
-            
-            return isRobotClicked ?
-                (
+    if (!robots.length) {
+        return <h1 className="tc">Loading . . .</h1>
+    } 
+    else if (filteredRobots.length === 1) {
+        const robotUser = filteredRobots[0];
+        
+        return isRobotClicked ?
+            (
+                <ErrorBoundary>
+                    <ProfileDisplay goBack={onGoBack} robotUser={robotUser} />
+                </ErrorBoundary> 
+            )
+                :
+            (
+                <ErrorBoundary>
+                    <CardsDisplay filteredRobots={filteredRobots} onSearchChange={onSearchChange} onCardClick={onCardClick} />
+                </ErrorBoundary>
+            )
+    } 
+    else {
+        return (
                     <ErrorBoundary>
-                        <ProfileDisplay goBack={this.onGoBack} robotUser={robotUser} />
-                    </ErrorBoundary> 
-                )
-                    :
-                (
-                    <ErrorBoundary>
-                        <CardsDisplay filteredRobots={filteredRobots} onSearchChange={this.onSearchChange} onCardClick={this.onCardClick} />
+                        <CardsDisplay filteredRobots={filteredRobots} onSearchChange={onSearchChange} onCardClick={onCardClick} />
                     </ErrorBoundary>
                 )
-        } 
-        else {
-            return (
-                        <ErrorBoundary>
-                            <CardsDisplay filteredRobots={filteredRobots} onSearchChange={this.onSearchChange} onCardClick={this.onCardClick} />
-                        </ErrorBoundary>
-                    )
-        }
     }
 }
 
